9b7b7522977155d279c2ed98ac96fccc55d95d98
frankye1000 <frankye100@gmail.com>
2022-11-08 12:25:51 +0800

    update mid e4

--- Exam/midterm/e4.c
+++ Exam/midterm/e4.c
@@ -5,44 +5,38 @@ int* Largest(int *arr, int size);
 void Swap(int *a, int *b);
 void Sort(int *arr, int size);
 
-int main(){
-
+int main()
+{
     int testcase;
     scanf("%d",&testcase);
-    while(testcase-- > 0){
+    while(testcase-- > 0)
+    {
         int temp,size=0;
         int A[10]={0};
-        while(scanf("%d",&temp) && temp!=0){
+        while(scanf("%d",&temp) && temp!=0)
+        {
             A[size]=temp;
             size++;
         }
-        // printf("size = %d | ",size);
-        // for(int i=0;i<size;i++){
-        //     printf("%d | ",A[i]);
-        // }
-        int *LP;
-        // LP = Largest(A,size);
-        // printf("%d", *LP);
-        Sort(A,size);
         
-        for(int i=0;i<size;i++){
-            printf("[%d] ",A[i]);
-        }
-
+        Sort(A,size);
+        for(int i=0;i<size;i++) printf("%d ",A[i]);
+        printf("\n");
     }
-
-
     return 0;
 }
 
 
-int* Largest(int *arr, int size){
+int* Largest(int *arr, int size)
+{
     int *p;
     int max=arr[0];
     int *rep=&arr[0];
     
-    for(p=arr; p<arr+size; p++){
-        if(*p > max){
+    for(p=arr; p<arr+size; p++)
+    {
+        if(*p > max)
+        {
             max = *p;
             rep = p;
         }
@@ -50,25 +44,22 @@ int* Largest(int *arr, int size){
     return rep;
 }
 
-void Swap(int *a, int *b){
+void Swap(int *a, int *b)
+{
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-void Sort(int *arr, int size){
-    
+void Sort(int *arr, int size)
+{
     int *p;
     int *LP;
     int SS = size;
-    for(p=arr; p<arr+size; p++){
+    for(p=arr; p<arr+size; p++)
+    {
         LP = Largest(p, SS);
         Swap(LP,p);
         SS--;
     }
-
-    
-
-
-
 }
