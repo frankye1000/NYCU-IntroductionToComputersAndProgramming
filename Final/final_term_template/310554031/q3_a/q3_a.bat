a96217e1177fcb9f752917a7d65e002a9af62fd5
frankye1000 <frankye100@gmail.com>
2023-01-14 10:33:20 +0800

    update 20230114

---
+++ Final/final_term_template/310554031/q3_a/q3_a.bat
@@ -0,0 +1,2 @@
+powershell.exe -Command "Get-Content .\src\test.txt | .\term_ans_3a.exe"
+pause
--- Final/final_term_template/310554031/q3_a/src/main.cpp
+++ Final/final_term_template/310554031/q3_a/src/main.cpp
@@ -1,9 +1,10 @@
-#include "q3_a.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 #define MAX_LEN 100000
 
-/*
-計算星期幾 基姆拉爾森計算公式
-*/
+/*計算星期幾 基姆拉爾森計算公式 https://www.itsfun.com.tw/%E5%9F%BA%E5%A7%86%E6%8B%89%E7%88%BE%E6%A3%AE%E8%A8%88%E7%AE%97%E5%85%AC%E5%BC%8F/wiki-5628372 */
 int week(int year, int month, int day){
     int y,y0,m,m0,d,d0,z,x;
     y = year;
@@ -18,75 +19,38 @@ int week(int year, int month, int day){
     return d0;
 }
 
-/*處理一般 case*/
-void sample(char* commend){
-    int month = 0;
-    int year  = 0;
-    int day   = 0;
-
-    //char Week[20]={'\0'};
-    const char s2[2] = "/";
-    char *token;
-
-    token = strtok(commend, s2);
-    year  = atoi(token);
-    
-    token = strtok(NULL, s2);
-    month = atoi(token);
-
-    token = strtok(NULL, s2);
-    day = atoi(token);
-
-    print_month(month);
-    printf("%d, ", day);
-    printf("%d ", year);
-    int w = week(year, month, day);
-    print_week(w);
-}
-
-/*處理ROC case*/
-void ROC_sample(char* commend){
-    int month = 0;
-    int year  = 0;
-    int day   = 0;
-
-    const char s1[2] = " ";
-    const char s2[2] = "/";
-    char *token;
-
-    token = strtok(commend, s1);
-
-    token = strtok(NULL, s2);
-    year  = atoi(token)+1911;
-    
-    token = strtok(NULL, s2);
-    month = atoi(token);
-
-    token = strtok(NULL, s2);
-    day = atoi(token);
-
-    print_month(month);
-    printf("%d, ", day);
-    printf("%d ", year);
-    int w = week(year, month, day);
-    print_week(w);
-}
-
-/*讀檔*/
-int read_input(char *commend){
-    int ch,i=0;
-    while((ch = getchar())!='\n'){
-        if(ch==EOF) return 0;
-        *(commend+i) = ch;
-        // printf("%c",ch);
-        i++;
+/*print星期幾*/
+void weekintTostring(int w){
+    switch (w)
+    {
+    case 1:
+        printf("(Monday)\n");
+        break;
+    case 2:
+        printf("(Tuesday)\n");
+        break;
+    case 3:
+        printf("(Wednesday)\n");
+        break;
+    case 4:
+        printf("(Thursday)\n");
+        break;
+    case 5:
+        printf("(Friday)\n");
+        break;
+    case 6:
+        printf("(Saturday)\n");
+        break;
+    case 0:
+        printf("(Sunday)\n");
+        break;
+    default:
+        break;
     }
-    *(commend+i)='\0';
-    return 1;
 }
 
-/*print月份*/
-void print_month(int month){
+/*print月份 int to string*/
+void monthintTostring(int month){
     switch (month){
         case 1:
             printf("January ");
@@ -129,36 +93,79 @@ void print_month(int month){
     }
 }
 
+/*處理一般 case*/
+void normal_token(char* commend){
+    int month = 0;
+    int year  = 0;
+    int day   = 0;
 
-/*print星期幾*/
-void print_week(int w){
-    switch (w)
-    {
-    case 1:
-        printf("(Monday)\n");
-        break;
-    case 2:
-        printf("(Tuesday)\n");
-        break;
-    case 3:
-        printf("(Wednesday)\n");
-        break;
-    case 4:
-        printf("(Thursday)\n");
-        break;
-    case 5:
-        printf("(Friday)\n");
-        break;
-    case 6:
-        printf("(Saturday)\n");
-        break;
-    case 0:
-        printf("(Sunday)\n");
-        break;
-    default:
-        break;
+    const char s2[2] = "/";
+    char *token;
+    /* 獲取第一個字字符串 */
+    token = strtok(commend, s2);
+    /* 繼續獲取其他的字字符串 */
+    int N=0;
+    while( token != NULL ){
+        if(N==0) year = atoi(token); 
+        else if(N==1) month = atoi(token);
+        else day = atoi(token);
+        N++;
+        token = strtok(NULL, s2);
     }
+
+    monthintTostring(month);
+    printf("%d, ", day);
+    printf("%d ", year);
+    int w = week(year, month, day);
+    weekintTostring(w);
 }
+
+/*處理ROC case*/
+void ROC_token(char* commend){
+    char* temp;
+    int month = 0;
+    int year  = 0;
+    int day   = 0;
+
+    const char s[3] = " /";
+    char *token;
+
+    /* 獲取第一個字字符串 */
+    token = strtok(commend, s);
+    /* 繼續獲取其他的字字符串 */
+    int N=0;
+    while( token != NULL ){
+        //printf("token = %s\n",token);
+        if(N==0) temp=token;  // ROC 
+        else if(N==1) year = atoi(token)+1911; 
+        else if(N==2) month = atoi(token);
+        else day = atoi(token);
+        N++;
+        token = strtok(NULL, s);
+    }
+
+    monthintTostring(month);
+    printf("%d, ", day);
+    printf("%d ", year);
+    int w = week(year, month, day);
+    weekintTostring(w);
+}
+
+
+
+int read_input(char *commend){
+    int ch,i=0;
+    while((ch = getchar())!='\n'){
+        if(ch==EOF) return 0;
+        *(commend+i) = ch;
+        // printf("%c",ch);
+        i++;
+    }
+    *(commend+i)='\0';
+    return 1;
+}
+
+
 
 
 int main(){
@@ -166,10 +173,10 @@ int main(){
 
     while(read_input(arr_char)){
         if(arr_char[0]=='R'){
-            ROC_sample(arr_char);
+            ROC_token(arr_char);
         }
         else{
-            sample(arr_char);
+            normal_token(arr_char);
         }
     }
     
